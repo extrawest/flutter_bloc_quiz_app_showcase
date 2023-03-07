e47cb14cc9d9a97e571d9483b9dbf6592fb09755
Maksym Ostrovyi <maksym.ostrovyj@extrawest.com>
2023-03-07 11:59:18 +0200

    local link for graphics asset added

--- lib/common/assets.dart
+++ lib/common/assets.dart
@@ -2,3 +2,4 @@
 const String credentialsDevFile = 'assets/config_dev.json';
 const String credentialsProdFile = 'assets/config_production.json';
 const String translationsFolderPath = 'assets/i18n';
+const String quizLogo = 'assets/images/quiz_splash.png';
