2a0864952de4f8dbb02cffae3aff292aa3a6fff8
Maksym Ostrovyi <maksym.ostrovyj@extrawest.com>
2023-03-08 16:46:15 +0200

    drawer_widget.dart added

---
+++ lib/common/widgets/drawer_widget.dart
@@ -0,0 +1,57 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+
+import '../../features/theme/theme_info.dart';
+import '../../generated/locale_keys.g.dart';
+
+class DrawerWidget extends StatelessWidget {
+  const DrawerWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.zero),
+      ),
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          DrawerHeader(
+            padding: EdgeInsets.zero,
+            child: ColoredBox(
+              color: blue,
+              child: Padding(
+                padding: const EdgeInsets.only(top: 16,left: 16),
+                child: Text(tr(LocaleKeys.quiz_menu)),
+              ),
+            ),
+          ),
+          ListTile(
+            title: Text(tr(LocaleKeys.true_false_quiz)),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            title: Text(tr(LocaleKeys.one_answer_quiz)),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            title: Text(tr(LocaleKeys.history)),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            title: Text(tr(LocaleKeys.settings)),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
