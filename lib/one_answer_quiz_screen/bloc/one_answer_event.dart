8e25e1a8806c0fa8a4fc9be76ed53dfb51661600
Maksym Ostrovyi <maksym.ostrovyj@extrawest.com>
2023-03-15 18:42:46 +0200

    deleted

--- lib/one_answer_quiz_screen/bloc/one_answer_event.dart
+++
@@ -1,4 +0,0 @@
-part of 'one_answer_bloc.dart';
-
-@immutable
-abstract class OneAnswerEvent {}
--- lib/one_answer_quiz_screen/bloc/one_answer_state.dart
+++
@@ -1,6 +0,0 @@
-part of 'one_answer_bloc.dart';
-
-@immutable
-abstract class OneAnswerState {}
-
-class OneAnswerInitial extends OneAnswerState {}
