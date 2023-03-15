f758667d2593d1cb22b2870fa1aa48d21c1d7e95
Maksym Ostrovyi <maksym.ostrovyj@extrawest.com>
2023-03-15 18:47:18 +0200

    true_false_quiz_screen.dart added

---
+++ lib/features/true_false_quiz/view/true_false_quiz_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TrueFalseQuizScreen extends StatelessWidget {
+  const TrueFalseQuizScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
