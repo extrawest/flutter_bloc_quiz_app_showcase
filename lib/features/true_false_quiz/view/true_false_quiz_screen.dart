bcbfd69d3acffeaa351d80aa4f3b850da3a21a63
Maksym Ostrovyi <maksym.ostrovyj@extrawest.com>
2023-03-21 15:59:17 +0200

    TrueFalseQuizScreen placeholder replaced with real one

--- lib/features/true_false_quiz/view/true_false_quiz_screen.dart
+++ lib/features/true_false_quiz/view/true_false_quiz_screen.dart
@@ -1,10 +1,36 @@
+import 'package:bloc_quiz_training/features/true_false_quiz/bloc/true_false_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'true_false_quiz_view_widget.dart';
 
 class TrueFalseQuizScreen extends StatelessWidget {
-  const TrueFalseQuizScreen({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('True/False Quiz'),
+        centerTitle: true,
+      ),
+      body: BlocProvider(
+        create: (_) =>
+            TrueFalseBloc(trueFalseQuizRepository: RepositoryProvider.of(context))..add(TrueFalseFetchEvent()),
+        child: BlocBuilder<TrueFalseBloc, TrueFalseState>(
+          builder: (context, state) {
+            switch (state.status) {
+              case TrueFalseStatus.failure:
+                return const Center(child: Text('failed to fetch questions'));
+              case TrueFalseStatus.success:
+                if (state.quizQuestions.isEmpty) {
+                  return const Center(child: Text('no questions'));
+                }
+                return const TrueFalseQuizView();
+              default:
+                return const Center(child: CircularProgressIndicator());
+            }
+          },
+        ),
+      ),
+    );
   }
 }
