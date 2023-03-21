0c4b180a46a20931cc83b6b1e203047a92951fce
Maksym Ostrovyi <maksym.ostrovyj@extrawest.com>
2023-03-21 16:00:50 +0200

    NextQuestionEvent and AnswerOnQuestionEvent added

--- lib/features/true_false_quiz/bloc/true_false_event.dart
+++ lib/features/true_false_quiz/bloc/true_false_event.dart
@@ -1,3 +1,21 @@
 part of 'true_false_bloc.dart';
 
-abstract class TrueFalseEvent {}
+abstract class TrueFalseEvent extends Equatable {
+  @override
+  List<Object> get props => [];
+}
+
+class TrueFalseFetchEvent extends TrueFalseEvent {}
+
+class NextQuestionEvent extends TrueFalseEvent {
+  final int? actualQuestion;
+
+  NextQuestionEvent(this.actualQuestion);
+}
+
+class AnswerOnQuestionEvent extends TrueFalseEvent {
+  final int id;
+  final bool answer;
+
+  AnswerOnQuestionEvent({required this.id, required this.answer});
+}
