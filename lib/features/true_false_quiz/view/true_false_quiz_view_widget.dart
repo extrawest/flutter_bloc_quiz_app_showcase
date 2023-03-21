7bbb6ea34c246c3c049dd99d5a1340b6254a15aa
Maksym Ostrovyi <maksym.ostrovyj@extrawest.com>
2023-03-21 15:56:14 +0200

    TrueFalseQuizViewWidget added

---
+++ lib/features/true_false_quiz/view/true_false_quiz_view_widget.dart
@@ -0,0 +1,41 @@
+import 'package:bloc_quiz_training/features/true_false_quiz/view/result_widget.dart';
+import 'package:bloc_quiz_training/features/true_false_quiz/view/true_false_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../common/widgets/base_button.dart';
+import '../bloc/true_false_bloc.dart';
+
+class TrueFalseQuizView extends StatelessWidget {
+  const TrueFalseQuizView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<TrueFalseBloc, TrueFalseState>(builder: (context, state) {
+      if (state.actualQuestion == null) {
+        return Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              const Text('Are you ready?'),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: BaseButton(
+                    onPressed: () => context.read<TrueFalseBloc>().add(NextQuestionEvent(state.actualQuestion)),
+                    isInternetConnected: true,
+                    isEnabled: true,
+                    buttonColor: Colors.greenAccent,
+                    title: 'GO!'),
+              ),
+            ],
+          ),
+        );
+      }
+      if (state.actualQuestion! < state.quizQuestions.length) {
+        return TrueFalseWidget(quiz: state.quizQuestions[state.actualQuestion!]);
+      } else {
+        return const ResultsWidget();
+      }
+    });
+  }
+}
