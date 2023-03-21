a38d27207d7cbeedeb3d4d0fcc2c82c918aad302
Maksym Ostrovyi <maksym.ostrovyj@extrawest.com>
2023-03-21 15:57:07 +0200

    some fields added to the true/false quiz state

--- lib/features/true_false_quiz/bloc/true_false_state.dart
+++ lib/features/true_false_quiz/bloc/true_false_state.dart
@@ -1,5 +1,38 @@
 part of 'true_false_bloc.dart';
 
-abstract class TrueFalseState {}
+enum TrueFalseStatus { initial, success, failure }
+
+class TrueFalseState extends Equatable {
+  final TrueFalseStatus status;
+  final List<TrueFalseQuiz> quizQuestions;
+  final String? errorMessage;
+  final int? actualQuestion;
+  final List<Map<String, dynamic>> answeredQuestions;
+
+  const TrueFalseState(
+      {this.status = TrueFalseStatus.initial,
+      this.quizQuestions = const <TrueFalseQuiz>[],
+      this.errorMessage,
+      this.actualQuestion,
+      this.answeredQuestions = const []});
+
+  TrueFalseState copyWith(
+      {TrueFalseStatus? status,
+      List<TrueFalseQuiz>? quizQuestions,
+      bool? hasReachedMax,
+      String? errorMessage,
+      int? actualQuestion,
+      List<Map<String, dynamic>>? answeredQuestions}) {
+    return TrueFalseState(
+        status: status ?? this.status,
+        quizQuestions: quizQuestions ?? this.quizQuestions,
+        errorMessage: errorMessage ?? this.errorMessage,
+        actualQuestion: actualQuestion ?? this.actualQuestion,
+        answeredQuestions: answeredQuestions ?? this.answeredQuestions);
+  }
+
+  @override
+  List<Object?> get props => [status, quizQuestions, actualQuestion, answeredQuestions];
+}
 
 class TrueFalseInitial extends TrueFalseState {}
