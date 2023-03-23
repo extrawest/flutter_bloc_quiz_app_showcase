8bdf4fbc7b68ac2ebb1f18966fd98817d5e3de00
Maksym Ostrovyi <maksym.ostrovyj@extrawest.com>
2023-03-23 11:28:20 +0200

    OneAnswerQuizView widget added

---
+++ lib/features/one_answer_quiz/view/one_answer_quiz_view.dart
@@ -0,0 +1,41 @@
+import 'package:bloc_quiz_training/features/one_answer_quiz/bloc/one_answer_bloc.dart';
+import 'package:bloc_quiz_training/features/one_answer_quiz/view/one_answer_quiz_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../common/widgets/base_button.dart';
+import 'one_answer_result_widget.dart';
+
+class OneAnswerQuizView extends StatelessWidget {
+  const OneAnswerQuizView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<OneAnswerBloc, OneAnswerState>(builder: (context, state) {
+      if (state.actualQuestion == null) {
+        return Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              const Text('Are you ready?'),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: BaseButton(
+                    onPressed: () => context.read<OneAnswerBloc>().add(NextQuestionEvent(state.actualQuestion)),
+                    isInternetConnected: true,
+                    isEnabled: true,
+                    buttonColor: Colors.greenAccent,
+                    title: 'GO!'),
+              ),
+            ],
+          ),
+        );
+      }
+      if (state.actualQuestion! < state.quizQuestions.length) {
+        return OneAnswerQuizWidget(quiz: state.quizQuestions[state.actualQuestion!]);
+      } else {
+        return const OneAnswerResultsWidget();
+      }
+    });
+  }
+}
