71675647414f3700eeec266b7f27d2a9ef0548a4
Maksym Ostrovyi <maksym.ostrovyj@extrawest.com>
2023-03-23 11:27:59 +0200

    NextQuestionEvent and AnsweredOnQuestionEvent added

--- lib/features/one_answer_quiz/bloc/one_answer_event.dart
+++ lib/features/one_answer_quiz/bloc/one_answer_event.dart
@@ -1,10 +1,21 @@
 part of 'one_answer_bloc.dart';
 
-
 abstract class OneAnswerEvent extends Equatable {
   @override
   List<Object> get props => [];
 }
 
-
 class OneAnswerFetchEvent extends OneAnswerEvent {}
+
+class NextQuestionEvent extends OneAnswerEvent {
+  final int? actualQuestion;
+
+  NextQuestionEvent(this.actualQuestion);
+}
+
+class AnswerOnQuestionEvent extends OneAnswerEvent {
+  final int id;
+  final String answer;
+
+  AnswerOnQuestionEvent({required this.id, required this.answer});
+}
