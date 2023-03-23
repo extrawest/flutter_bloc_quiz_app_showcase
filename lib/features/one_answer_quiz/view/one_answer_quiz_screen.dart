e9acd0a8252afc4833fb7ebc475300c894adc2cb
Maksym Ostrovyi <maksym.ostrovyj@extrawest.com>
2023-03-23 11:31:40 +0200

    cubit initialization added on OneAnswerQuizScreen

--- lib/features/one_answer_quiz/view/one_answer_quiz_screen.dart
+++ lib/features/one_answer_quiz/view/one_answer_quiz_screen.dart
@@ -1,36 +1,53 @@
 import 'package:bloc_quiz_training/features/one_answer_quiz/bloc/one_answer_bloc.dart';
+import 'package:bloc_quiz_training/features/one_answer_quiz/cubit/one_answer_button_cubit.dart';
+import 'package:bloc_quiz_training/features/one_answer_quiz/view/one_answer_quiz_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class OneAnswerQuizScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('One Answer')),
-      body: BlocProvider(
-        create: (_) =>
-            OneAnswerBloc(oneAnswerQuizRepository: RepositoryProvider.of(context))..add(OneAnswerFetchEvent()),
-        child: BlocBuilder<OneAnswerBloc, OneAnswerState>(
-          builder: (context, state) {
-            switch (state.status) {
-              case OneAnswerStatus.failure:
-                return const Center(child: Text('failed to fetch questiona'));
-              case OneAnswerStatus.success:
-                if (state.quizQuestions.isEmpty) {
-                  return const Center(child: Text('no questions'));
-                }
-                return ListView.builder(
-                  itemBuilder: (BuildContext context, int index) {
-                    return Text(state.quizQuestions[index].question ?? 'something went wrong!');
+    return MultiBlocProvider(
+        providers: [
+          BlocProvider(
+              create: (_) =>
+              OneAnswerBloc(oneAnswerQuizRepository: RepositoryProvider.of(context))
+                ..add(OneAnswerFetchEvent())),
+          BlocProvider(
+            create: (context) => OneAnswerButtonCubit(),
+          )
+        ],
+        child: Scaffold(
+          appBar: AppBar(
+            leading: BlocBuilder<OneAnswerButtonCubit, OneAnswerButtonState>(
+              builder: (context, state) {
+                return IconButton(
+                  onPressed: () {
+                    context.read<OneAnswerButtonCubit>().unselectAction();
+                    Navigator.pop(context);
                   },
-                  itemCount: state.quizQuestions.length,
+                  icon: const Icon(Icons.arrow_back_rounded),
                 );
-              default:
-                return const Center(child: CircularProgressIndicator());
-            }
-          },
-        ),
-      ),
-    );
+              },
+            ),
+            title: const Text('One Answer'),
+            automaticallyImplyLeading: false,
+          ),
+          body: BlocBuilder<OneAnswerBloc, OneAnswerState>(
+            builder: (context, state) {
+              switch (state.status) {
+                case OneAnswerStatus.failure:
+                  return const Center(child: Text('failed to fetch questions'));
+                case OneAnswerStatus.success:
+                  if (state.quizQuestions.isEmpty) {
+                    return const Center(child: Text('no questions'));
+                  }
+                  return const OneAnswerQuizView();
+                default:
+                  return const Center(child: CircularProgressIndicator());
+              }
+            },
+          ),
+        ));
   }
 }
