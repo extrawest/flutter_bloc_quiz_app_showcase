f766f5d1097d7570e1023f21df23ac6ddf7f082b
Maksym Ostrovyi <maksym.ostrovyj@extrawest.com>
2023-03-23 11:29:39 +0200

    OneAnswerResultsWidget added

---
+++ lib/features/one_answer_quiz/view/one_answer_result_widget.dart
@@ -0,0 +1,43 @@
+import 'package:bloc_quiz_training/features/one_answer_quiz/bloc/one_answer_bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class OneAnswerResultsWidget extends StatelessWidget {
+  const OneAnswerResultsWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<OneAnswerBloc, OneAnswerState>(builder: (context, state) {
+      return Column(
+        children: [
+          const Padding(
+            padding: EdgeInsets.symmetric(vertical: 20),
+            child: Text(
+              'Quiz Results',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemBuilder: (BuildContext context, int index) {
+                return Container(
+                  height: 60,
+                  decoration: BoxDecoration(
+                      color: state.quizQuestions[index].rightAnswer == state.answeredQuestions[index]['answer']
+                          ? Colors.green
+                          : Colors.red,
+                      borderRadius: BorderRadius.circular(8),
+                      border: Border.all(color: Colors.black)),
+                  margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+                  padding: const EdgeInsets.all(4),
+                  child: Center(child: Text(state.quizQuestions[index].question!)),
+                );
+              },
+              itemCount: state.quizQuestions.length,
+            ),
+          ),
+        ],
+      );
+    });
+  }
+}
