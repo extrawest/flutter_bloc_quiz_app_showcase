fc49d3a0e49524b59bfaa26e41dab064bc9284c5
Maksym Ostrovyi <maksym.ostrovyj@extrawest.com>
2023-03-30 11:57:22 +0300

    OneAnswerCubitState added

---
+++ lib/features/one_answer_quiz/cubit/one_answer_state.dart
@@ -0,0 +1,22 @@
+part of 'one_answer_cubit.dart';
+
+@immutable
+abstract class OneAnswerCubitState extends Equatable{
+  final int? index;
+  const OneAnswerCubitState({this.index});
+}
+
+class OneAnswerButtonInitial extends OneAnswerCubitState {
+  const OneAnswerButtonInitial({super.index});
+
+  @override
+  List<Object?> get props => [index];
+}
+
+class OneAnswerButtonSelected extends OneAnswerCubitState {
+   const OneAnswerButtonSelected({super.index});
+
+  @override
+  List<Object?> get props => [index];
+}
+
