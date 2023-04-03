e67d7472cb1c5f4c86ea2afe721b1bae1c20d703
Maksym Ostrovyi <maksym.ostrovyj@extrawest.com>
2023-04-03 15:22:56 +0300

    HistoryScreen added to the Drawer

--- lib/common/widgets/drawer_widget.dart
+++ lib/common/widgets/drawer_widget.dart
@@ -1,7 +1,9 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 
+import '../../features/storage/quiz_storage_bloc.dart';
 import '../../features/theme/theme_info.dart';
 import '../../generated/locale_keys.g.dart';
 import '../router.dart';
@@ -45,7 +47,9 @@ class DrawerWidget extends StatelessWidget {
           ListTile(
             title: Text(tr(LocaleKeys.history)),
             onTap: () {
+              context.read<QuizStorageBloc>().add(QuizStorageReadEvent());
               Navigator.pop(context);
+              context.push(historyScreenRoute);
             },
           ),
           ListTile(
