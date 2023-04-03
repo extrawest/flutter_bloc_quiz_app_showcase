d88fab1411eb5c9c3ba59b2ee39651791f35c361
Maksym Ostrovyi <maksym.ostrovyj@extrawest.com>
2023-04-03 16:19:39 +0300

    rightAnswers, wrongAnswers and quizType fields added

--- lib/features/one_answer_quiz/bloc/one_answer_state.dart
+++ lib/features/one_answer_quiz/bloc/one_answer_state.dart
@@ -7,6 +7,9 @@ class OneAnswerState extends Equatable {
   final List<OneAnswerQuiz> quizQuestions;
   final String? errorMessage;
   final int? actualQuestion;
+  final int rightAnswers;
+  final int wrongAnswers;
+  final QuizType? quizType;
   final List<Map<String, dynamic>> answeredQuestions;
 
   const OneAnswerState(
@@ -14,6 +17,9 @@ class OneAnswerState extends Equatable {
       this.quizQuestions = const <OneAnswerQuiz>[],
       this.errorMessage,
       this.actualQuestion,
+      this.rightAnswers = 0,
+      this.wrongAnswers = 0,
+      this.quizType = QuizType.none,
       this.answeredQuestions = const []});
 
   OneAnswerState copyWith(
@@ -22,13 +28,19 @@ class OneAnswerState extends Equatable {
       bool? hasReachedMax,
       String? errorMessage,
       int? actualQuestion,
+      int? rightAnswers,
+      int? wrongAnswers,
+      QuizType? quizType,
       List<Map<String, dynamic>>? answeredQuestions}) {
     return OneAnswerState(
         status: status ?? this.status,
         quizQuestions: quizQuestions ?? this.quizQuestions,
         errorMessage: errorMessage ?? this.errorMessage,
         actualQuestion: actualQuestion ?? this.actualQuestion,
-        answeredQuestions: answeredQuestions ?? this.answeredQuestions);
+        rightAnswers: rightAnswers ?? this.rightAnswers,
+        wrongAnswers: wrongAnswers ?? this.wrongAnswers,
+        answeredQuestions: answeredQuestions ?? this.answeredQuestions,
+        quizType: quizType ?? this.quizType);
   }
 
   @override
