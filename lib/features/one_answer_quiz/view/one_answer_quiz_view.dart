978480e240f404c4402490d61a0f3906cff17504
Maksym Ostrovyi <maksym.ostrovyj@extrawest.com>
2023-04-06 11:34:50 +0300

    placeholder screen replaced with real one

--- lib/features/one_answer_quiz/view/one_answer_quiz_view.dart
+++ lib/features/one_answer_quiz/view/one_answer_quiz_view.dart
@@ -1,11 +1,14 @@
 import 'package:bloc_quiz_training/features/one_answer_quiz/bloc/one_answer_bloc.dart';
 import 'package:bloc_quiz_training/features/one_answer_quiz/view/one_answer_quiz_widget.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../common/widgets/base_button.dart';
+import '../../../generated/locale_keys.g.dart';
 import '../../storage/data_models/quiz_result_model.dart';
 import '../../storage/quiz_storage_bloc.dart';
+import '../../theme/theme_info.dart';
 import 'one_answer_result_widget.dart';
 
 class OneAnswerQuizView extends StatelessWidget {
@@ -16,20 +19,35 @@ class OneAnswerQuizView extends StatelessWidget {
     return BlocBuilder<OneAnswerBloc, OneAnswerState>(builder: (context, state) {
       if (state.actualQuestion == null) {
         return Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              const Text('Are you ready?'),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16),
-                child: BaseButton(
-                    onPressed: () => context.read<OneAnswerBloc>().add(NextQuestionEvent(state.actualQuestion)),
-                    isInternetConnected: true,
-                    isEnabled: true,
-                    buttonColor: Colors.greenAccent,
-                    title: 'GO!'),
-              ),
-            ],
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 10),
+                  child: Text(
+                    tr(LocaleKeys.one_answer_description),
+                    style: text,
+                    textAlign: TextAlign.justify,
+                  ),
+                ),
+                Text(
+                  tr(LocaleKeys.are_you_ready),
+                  style: notifierTextLabel,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  child: BaseButton(
+                      onPressed: () => context.read<OneAnswerBloc>().add(NextQuestionEvent(state.actualQuestion)),
+                      isInternetConnected: true,
+                      isEnabled: true,
+                      buttonColor: Colors.green,
+                      titleColor: Theme.of(context).colorScheme.primary,
+                      title: tr(LocaleKeys.go)),
+                ),
+              ],
+            ),
           ),
         );
       }
@@ -37,13 +55,13 @@ class OneAnswerQuizView extends StatelessWidget {
         return OneAnswerQuizWidget(quiz: state.quizQuestions[state.actualQuestion!]);
       } else {
         context.read<QuizStorageBloc>().add(QuizStorageWriteEvent(
-          savedQuiz: SavedQuiz(
-            date: DateTime.now().millisecondsSinceEpoch,
-            rightAnswers: state.rightAnswers,
-            wrongAnswers: state.wrongAnswers,
-            quizType: QuizType.oneAnswer,
-          ),
-        ));
+              savedQuiz: SavedQuiz(
+                date: DateTime.now().millisecondsSinceEpoch,
+                rightAnswers: state.rightAnswers,
+                wrongAnswers: state.wrongAnswers,
+                quizType: QuizType.oneAnswer,
+              ),
+            ));
         return const OneAnswerResultsWidget();
       }
     });
