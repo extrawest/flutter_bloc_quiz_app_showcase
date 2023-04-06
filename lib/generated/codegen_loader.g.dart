2f0af57ac9c83b3928b7326db022e2836de67c16
Maksym Ostrovyi <maksym.ostrovyj@extrawest.com>
2023-04-06 11:43:39 +0300

    localized content added, local keys generated

--- lib/generated/codegen_loader.g.dart
+++ lib/generated/codegen_loader.g.dart
@@ -42,7 +42,24 @@ class CodegenLoader extends AssetLoader{
   "one_answer_quiz": "Одна відповідь",
   "history": "Минулі опитування",
   "quiz_menu": "Опитування",
-  "settings": "Налаштування"
+  "settings": "Налаштування",
+  "true_false_description": "Запрошуємо вас пройти короткий тест. Просто виберіть «Правда» або «Хибно». Після завершення ви зможете побачити правильні відповіді на екрані результатів.",
+  "one_answer_description": "Запрошуємо вас пройти короткий тест. Спочатку оберіть відповідь, потім натисніть кнопку «Перевірити». Після завершення ви зможете побачити правильні відповіді на екрані результатів.",
+  "true_key": "Правда",
+  "false_key": "Брехня",
+  "something_went_wrong": "Щось пішло не так",
+  "are_you_ready": "Ви готові?",
+  "go": "ПОЇХАЛИ!",
+  "quiz_results": "Результати",
+  "done": "Завершити",
+  "validate": "Натисніть, щоб перевірити",
+  "validating": "Перевірка...",
+  "wrong_answers": "неправильні відповіді",
+  "right_answers": "правильні відповіді",
+  "one_answer": "Одна відповідь",
+  "true_false": "Правда-Брехня",
+  "have_no_history": "У вас щє немає історії",
+  "brightness_theme": "Тема"
 };
 static const Map<String,dynamic> en_US = {
   "app_bar": {
@@ -72,7 +89,24 @@ static const Map<String,dynamic> en_US = {
   "one_answer_quiz": "One Answer Quiz",
   "history": "History",
   "quiz_menu": "Quiz menu",
-  "settings": "Settings"
+  "settings": "Settings",
+  "true_false_description": "We invite you to take a quick quiz. Just choose «True» or «False». Once completed, you will be able to see the correct answers on the results screen.",
+  "one_answer_description": "We invite you to take a short quiz. First, choose an answer, then click the «Tap to validate answer» button. Once completed, you will be able to see the correct answers on the results screen.",
+  "true_key": "True",
+  "false_key": "False",
+  "something_went_wrong": "Something went wrong",
+  "are_you_ready": "Are you ready",
+  "go": "GO!",
+  "quiz_results": "Quiz Results",
+  "done": "Done",
+  "validate": "Tap to validate answer",
+  "validating": "Validating...",
+  "wrong_answers": "wrong answers",
+  "right_answers": "right answers",
+  "one_answer": "One Answer",
+  "true_false": "True-False",
+  "have_no_history": "You have no history",
+  "brightness_theme": "Theme"
 };
 static const Map<String, Map<String,dynamic>> mapLocales = {"uk_UA": uk_UA, "en_US": en_US};
 }
--- lib/generated/locale_keys.g.dart
+++ lib/generated/locale_keys.g.dart
@@ -21,5 +21,22 @@ abstract class  LocaleKeys {
   static const history = 'history';
   static const quiz_menu = 'quiz_menu';
   static const settings = 'settings';
+  static const true_false_description = 'true_false_description';
+  static const one_answer_description = 'one_answer_description';
+  static const true_key = 'true_key';
+  static const false_key = 'false_key';
+  static const something_went_wrong = 'something_went_wrong';
+  static const are_you_ready = 'are_you_ready';
+  static const go = 'go';
+  static const quiz_results = 'quiz_results';
+  static const done = 'done';
+  static const validate = 'validate';
+  static const validating = 'validating';
+  static const wrong_answers = 'wrong_answers';
+  static const right_answers = 'right_answers';
+  static const one_answer = 'one_answer';
+  static const true_false = 'true_false';
+  static const have_no_history = 'have_no_history';
+  static const brightness_theme = 'brightness_theme';
 
 }
