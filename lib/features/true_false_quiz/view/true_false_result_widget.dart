c99de7c8feca54b0a01d74e157a137b3e220e729
Maksym Ostrovyi <maksym.ostrovyj@extrawest.com>
2023-04-06 11:42:41 +0300

    strings replaced with localized content

--- lib/features/true_false_quiz/view/true_false_result_widget.dart
+++ lib/features/true_false_quiz/view/true_false_result_widget.dart
@@ -1,3 +1,6 @@
+import 'package:bloc_quiz_training/common/widgets/quiz_result.dart';
+import 'package:bloc_quiz_training/generated/locale_keys.g.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
@@ -12,42 +15,43 @@ class TrueFalseResultsWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocBuilder<TrueFalseBloc, TrueFalseState>(builder: (context, state) {
       return Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          const Padding(
-            padding: EdgeInsets.symmetric(vertical: 20),
-            child: Text(
-              'Quiz Results',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-          ),
           Expanded(
-            child: ListView.builder(
-              itemBuilder: (BuildContext context, int index) {
-                return Container(
-                  height: 60,
-                  decoration: BoxDecoration(
-                      color: state.quizQuestions[index].rightAnswer == state.answeredQuestions[index]['answer']
-                          ? Colors.green
-                          : Colors.red,
-                      borderRadius: BorderRadius.circular(8),
-                      border: Border.all(color: Colors.black)),
-                  margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
-                  padding: const EdgeInsets.all(4),
-                  child: Center(child: Text(state.quizQuestions[index].question!)),
-                );
-              },
-              itemCount: state.quizQuestions.length,
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 20),
+                  child: Text(
+                    tr(LocaleKeys.quiz_results),
+                    style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Expanded(
+                  child: ListView.builder(
+                    itemBuilder: (BuildContext context, int index) {
+                      return QuizResult(
+                        isAnswerRight:
+                            state.quizQuestions[index].rightAnswer == state.answeredQuestions[index]['answer'],
+                        question: state.quizQuestions[index].question!,
+                      );
+                    },
+                    itemCount: state.quizQuestions.length,
+                  ),
+                ),
+              ],
             ),
           ),
-          Expanded(child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
             child: BaseButton(
                 isInternetConnected: true,
                 isEnabled: true,
                 buttonColor: Colors.green,
                 onPressed: context.pop,
-                title: 'Done!'),
-          ))
+                titleColor: Theme.of(context).colorScheme.primary,
+                title: tr(LocaleKeys.done)),
+          )
         ],
       );
     });
