0262b1382896d76c9457e2e4b58fd3f35e70e58d
Maksym Ostrovyi <maksym.ostrovyj@extrawest.com>
2023-04-06 11:35:23 +0300

    realtime localization with google translate added

--- lib/features/one_answer_quiz/view/one_answer_quiz_widget.dart
+++ lib/features/one_answer_quiz/view/one_answer_quiz_widget.dart
@@ -1,9 +1,14 @@
 import 'package:bloc_quiz_training/features/one_answer_quiz/bloc/one_answer_bloc.dart';
 import 'package:bloc_quiz_training/features/one_answer_quiz/cubit/one_answer_cubit.dart';
 import 'package:bloc_quiz_training/features/one_answer_quiz/data_models/one_answer_quiz_data_model.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:recase/recase.dart';
+import 'package:translator/translator.dart';
 
+import '../../../generated/locale_keys.g.dart';
+import '../../theme/theme_info.dart';
 import 'bottom_button.dart';
 
 class OneAnswerQuizWidget extends StatefulWidget {
@@ -57,24 +62,50 @@ class _OneAnswerQuizWidgetState extends State<OneAnswerQuizWidget> with SingleTi
                   mainAxisAlignment: MainAxisAlignment.spaceAround,
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    Text(widget.quiz.question ?? 'Something went wrong'),
+                    FutureBuilder(
+                        future: getQuestion(Localizations.localeOf(context).languageCode, widget.quiz.question),
+                        builder: (context, asyncSnap) {
+                          if (asyncSnap.hasData) {
+                            return Text(
+                              asyncSnap.data!,
+                              style: text,
+                            );
+                          } else {
+                            return const Center(
+                              child: CircularProgressIndicator(),
+                            );
+                          }
+                        }),
                     Expanded(
-                      child: ListView.builder(
-                        itemCount: widget.quiz.answers!.length,
-                        itemBuilder: (BuildContext context, int index) {
-                          return GestureDetector(
-                            child: Padding(
-                              padding: const EdgeInsets.symmetric(vertical: 4),
-                              child: Text(
-                                widget.quiz.answers![index].answer!,
-                                style: TextStyle(color: state.index == index ? Colors.blue : Colors.black),
+                      child: Padding(
+                        padding: const EdgeInsets.only(top: 40),
+                        child: ListView.builder(
+                          itemCount: widget.quiz.answers!.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            return GestureDetector(
+                              child: Padding(
+                                padding: const EdgeInsets.symmetric(vertical: 8),
+                                child: FutureBuilder(
+                                    future: getAnswer(Localizations.localeOf(context).languageCode,
+                                        widget.quiz.answers![index].answer!),
+                                    builder: (context, asyncSnap) {
+                                      if (asyncSnap.hasData) {
+                                        return Text(asyncSnap.data!.titleCase,
+                                            style: text.copyWith(
+                                                color: state.index == index
+                                                    ? Colors.blue
+                                                    : Theme.of(context).colorScheme.primary));
+                                      } else {
+                                        return Container();
+                                      }
+                                    }),
                               ),
-                            ),
-                            onTap: () {
-                              buttonCubit.selectAnswer(index: index);
-                            },
-                          );
-                        },
+                              onTap: () {
+                                buttonCubit.selectAnswer(index: index);
+                              },
+                            );
+                          },
+                        ),
                       ),
                     )
                   ],
@@ -85,24 +116,68 @@ class _OneAnswerQuizWidgetState extends State<OneAnswerQuizWidget> with SingleTi
               alignment: Alignment.bottomCenter,
               child: SlideTransition(
                 position: offset,
-                child: BottomButton(
-                    title: state.index != null ? widget.quiz.answers![state.index!].answer! : 'Validating...',
-                    callBack: state.index != null
-                        ? () {
-                            context.read<OneAnswerBloc>()
-                              ..add(AnswerOnQuestionEvent(
-                                  id: widget.quiz.id!,
-                                  answer: widget.quiz.answers![state.index!].answer!,
-                                  rightAnswer: widget.quiz.rightAnswer!))
-                              ..add(NextQuestionEvent(context.read<OneAnswerBloc>().state.actualQuestion));
-                            buttonCubit.initial();
+                child: state.index != null
+                    ? FutureBuilder(
+                        future: getAnswer(
+                            Localizations.localeOf(context).languageCode, widget.quiz.answers![state.index!].answer!),
+                        builder: (context, asyncSnap) {
+                          if (asyncSnap.hasData) {
+                            return BottomButton(
+                                title: asyncSnap.data!.titleCase,
+                                callBack: () {
+                                  context.read<OneAnswerBloc>()
+                                    ..add(AnswerOnQuestionEvent(
+                                        id: widget.quiz.id!,
+                                        answer: widget.quiz.answers![state.index!].answer!,
+                                        rightAnswer: widget.quiz.rightAnswer!))
+                                    ..add(NextQuestionEvent(context.read<OneAnswerBloc>().state.actualQuestion));
+                                  buttonCubit.initial();
+                                });
+                          } else {
+                            return Container();
                           }
-                        : () {}),
+                        })
+                    : BottomButton(
+                        callBack: () {},
+                        title: tr(LocaleKeys.validating),
+                      ),
               ),
             )
           ],
         );
       },
     );
+  }
+
+  Future<String> getQuestion(String langCode, String? question) async {
+    if (question != null) {
+      String tempQuestion = question;
+      if (langCode == 'uk') {
+        await GoogleTranslator().translate(question, from: 'en', to: 'uk').then((value) {
+          tempQuestion = value.text;
+        });
+        return tempQuestion;
+      } else {
+        return question;
+      }
+    } else {
+      return tr(LocaleKeys.something_went_wrong);
+    }
+  }
+
+  Future<String> getAnswer(String langCode, String? answer) async {
+    if (answer != null) {
+      String tempQuestion = answer;
+      if (langCode == 'uk') {
+        await GoogleTranslator().translate(answer, from: 'en', to: 'uk').then((value) {
+          tempQuestion = value.text;
+        });
+        return tempQuestion;
+      } else {
+        return answer;
+      }
+    } else {
+      return tr(LocaleKeys.something_went_wrong);
+    }
   }
 }
