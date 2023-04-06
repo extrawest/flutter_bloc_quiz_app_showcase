33d07bcd0e4719c6fb9bd65246f7e86c44c7dd38
Maksym Ostrovyi <maksym.ostrovyj@extrawest.com>
2023-04-06 11:43:12 +0300

    realtime localization with google translate added

--- lib/features/true_false_quiz/view/true_false_widget.dart
+++ lib/features/true_false_quiz/view/true_false_widget.dart
@@ -1,8 +1,13 @@
 import 'package:bloc_quiz_training/common/widgets/base_button.dart';
 import 'package:bloc_quiz_training/features/true_false_quiz/bloc/true_false_bloc.dart';
 import 'package:bloc_quiz_training/features/true_false_quiz/data_models/true_false_quiz_data_model.dart';
+import 'package:bloc_quiz_training/generated/locale_keys.g.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:translator/translator.dart';
+
+import '../../theme/theme_info.dart';
 
 class TrueFalseQuestionWidget extends StatefulWidget {
   final TrueFalseQuiz quiz;
@@ -19,10 +24,26 @@ class _TrueFalseQuestionWidgetState extends State<TrueFalseQuestionWidget> {
       child: Padding(
         padding: const EdgeInsets.all(16),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Text(widget.quiz.question ?? 'Something went wrong'),
+            Expanded(
+              child: FutureBuilder(
+                  future: getQuestion(Localizations.localeOf(context).languageCode, widget.quiz.question),
+                  builder: (context, asyncSnap) {
+                    if (asyncSnap.hasData) {
+                      return Center(
+                          child: Text(
+                        asyncSnap.data!,
+                        style: text,
+                      ));
+                    } else {
+                      return const Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    }
+                  }),
+            ),
             Row(
               mainAxisSize: MainAxisSize.min,
               mainAxisAlignment: MainAxisAlignment.center,
@@ -38,7 +59,8 @@ class _TrueFalseQuestionWidgetState extends State<TrueFalseQuestionWidget> {
                               id: widget.quiz.id!, answer: true, rightAnswer: widget.quiz.rightAnswer!))
                           ..add(NextQuestionEvent(context.read<TrueFalseBloc>().state.actualQuestion));
                       },
-                      title: 'True'),
+                      titleColor: Theme.of(context).colorScheme.primary,
+                      title: tr(LocaleKeys.true_key)),
                 ),
                 const SizedBox(
                   width: 16,
@@ -54,7 +76,8 @@ class _TrueFalseQuestionWidgetState extends State<TrueFalseQuestionWidget> {
                               id: widget.quiz.id!, answer: false, rightAnswer: widget.quiz.rightAnswer!))
                           ..add(NextQuestionEvent(context.read<TrueFalseBloc>().state.actualQuestion));
                       },
-                      title: 'False'),
+                      titleColor: Theme.of(context).colorScheme.primary,
+                      title: tr(LocaleKeys.false_key)),
                 )
               ],
             )
@@ -62,5 +85,21 @@ class _TrueFalseQuestionWidgetState extends State<TrueFalseQuestionWidget> {
         ),
       ),
     );
+  }
+
+  Future<String> getQuestion(String langCode, String? question) async {
+    if (question != null) {
+      String tempQuestion = question;
+      if (langCode == 'uk') {
+        await GoogleTranslator().translate(question, from: 'en', to: 'uk').then((value) {
+          tempQuestion = value.text;
+        });
+        return tempQuestion;
+      } else {
+        return question;
+      }
+    } else {
+      return tr(LocaleKeys.something_went_wrong);
+    }
   }
 }
