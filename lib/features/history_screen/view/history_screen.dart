784b5b5e70b7132c62d16d90613b0a299151b108
Maksym Ostrovyi <maksym.ostrovyj@extrawest.com>
2023-04-06 11:31:21 +0300

    theming added, string literals replaced with localized content

--- lib/features/history_screen/view/history_screen.dart
+++ lib/features/history_screen/view/history_screen.dart
@@ -1,8 +1,10 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-import '../../common/utils/app_utils.dart';
-import '../../features/storage/quiz_storage_bloc.dart';
+import '../../../common/utils/app_utils.dart';
+import '../../../generated/locale_keys.g.dart';
+import '../../storage/quiz_storage_bloc.dart';
 
 class HistoryScreen extends StatelessWidget {
   const HistoryScreen({Key? key}) : super(key: key);
@@ -23,9 +25,9 @@ class HistoryScreen extends StatelessWidget {
                     margin: const EdgeInsets.symmetric(vertical: 4, horizontal: 16),
                     height: 60,
                     decoration: BoxDecoration(
-                        color: Colors.white,
+                        color: Theme.of(context).primaryColor,
                         borderRadius: BorderRadius.circular(8),
-                        border: Border.all(color: Colors.black)),
+                        border: Border.all(color: Theme.of(context).colorScheme.primary)),
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.spaceAround,
                       children: [
@@ -34,19 +36,21 @@ class HistoryScreen extends StatelessWidget {
                           mainAxisAlignment: MainAxisAlignment.center,
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            Text('wrong answers: ${state.quizList![index].wrongAnswers}'),
-                            Text('right answers: ${state.quizList![index].rightAnswers}'),
+                            Text('${tr(LocaleKeys.wrong_answers)}: ${state.quizList![index].wrongAnswers}'),
+                            Text('${tr(LocaleKeys.right_answers)}: ${state.quizList![index].rightAnswers}'),
                           ],
                         ),
-                        Text(state.quizList![index].quizType.isOneAnswerQuiz ? 'One Answer' : ' True-False')
+                        Text(state.quizList![index].quizType.isOneAnswerQuiz
+                            ? tr(LocaleKeys.one_answer)
+                            : tr(LocaleKeys.true_false))
                       ],
                     ),
                   );
                 },
                 itemCount: state.quizList?.length,
               )
-            : const Center(
-                child: Text('You have no history'),
+            : Center(
+                child: Text(tr(LocaleKeys.have_no_history)),
               );
       }),
     );
