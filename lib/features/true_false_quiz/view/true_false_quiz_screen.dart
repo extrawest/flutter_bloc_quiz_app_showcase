6ebb8166cef08c89b40063df03615fe4573dd95f
Maksym Ostrovyi <maksym.ostrovyj@extrawest.com>
2023-04-13 12:09:58 +0300

    TrueFalseTranslateCubit initialization added

--- lib/features/true_false_quiz/view/true_false_quiz_screen.dart
+++ lib/features/true_false_quiz/view/true_false_quiz_screen.dart
@@ -1,9 +1,11 @@
 import 'package:bloc_quiz_training/features/true_false_quiz/bloc/true_false_bloc.dart';
+import 'package:bloc_quiz_training/features/true_false_quiz/cubit/true_false_translate_cubit.dart';
 import 'package:bloc_quiz_training/generated/locale_keys.g.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../repositories/true_false_quiz_repository.dart';
 import 'true_false_quiz_view_widget.dart';
 
 class TrueFalseQuizScreen extends StatelessWidget {
@@ -14,9 +16,15 @@ class TrueFalseQuizScreen extends StatelessWidget {
         title: Text(tr(LocaleKeys.true_false_quiz)),
         centerTitle: true,
       ),
-      body: BlocProvider(
-        create: (_) =>
-            TrueFalseBloc(trueFalseQuizRepository: RepositoryProvider.of(context))..add(TrueFalseFetchEvent()),
+      body: MultiBlocProvider(
+        providers: [
+          BlocProvider(
+            create: (_) =>
+                TrueFalseBloc(trueFalseQuizRepository: RepositoryProvider.of<TrueFalseQuizRepositoryImpl>(context))
+                  ..add(TrueFalseFetchEvent()),
+          ),
+          BlocProvider(create: (context) => TrueFalseTranslateCubit())
+        ],
         child: BlocBuilder<TrueFalseBloc, TrueFalseState>(
           builder: (context, state) {
             switch (state.status) {
