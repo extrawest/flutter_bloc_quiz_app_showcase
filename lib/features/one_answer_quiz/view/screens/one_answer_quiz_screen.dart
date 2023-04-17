f8917d0510f2f0ba70df8d0a7de3c797c9a702cf
Maksym Ostrovyi <maksym.ostrovyj@extrawest.com>
2023-04-17 12:13:19 +0300

    moved to the screens directory

--- lib/features/one_answer_quiz/view/screens/one_answer_quiz_screen.dart
+++ lib/features/one_answer_quiz/view/screens/one_answer_quiz_screen.dart
@@ -1,12 +1,12 @@
 import 'package:bloc_quiz_training/features/one_answer_quiz/bloc/one_answer_bloc.dart';
 import 'package:bloc_quiz_training/features/one_answer_quiz/cubit/one_answer_cubit.dart';
-import 'package:bloc_quiz_training/features/one_answer_quiz/view/one_answer_quiz_view.dart';
+import 'package:bloc_quiz_training/features/one_answer_quiz/view/widgets/one_answer_quiz_view.dart';
 import 'package:bloc_quiz_training/generated/locale_keys.g.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-import '../repositories/one_answer_quiz_repository.dart';
+import '../../repositories/one_answer_quiz_repository.dart';
 
 class OneAnswerQuizScreen extends StatelessWidget {
   @override
