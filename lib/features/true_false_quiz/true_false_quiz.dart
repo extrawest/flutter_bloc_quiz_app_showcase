4b03c09ca08cd93f996f2c97fd48a0182082f133
Maksym Ostrovyi <maksym.ostrovyj@extrawest.com>
2023-04-17 13:40:45 +0300

    true_false_quiz added for true false quiz

---
+++ lib/features/true_false_quiz/true_false_quiz.dart
@@ -0,0 +1,7 @@
+export 'bloc/true_false_bloc.dart';
+export 'cubit/true_false_translate_cubit.dart';
+export 'data_models/true_false_quiz_data_model.dart';
+export 'repositories/true_false_quiz_repository.dart';
+export 'services/true_false_quiz_api_service.dart';
+export 'view/screens/true_false_quiz_screen.dart';
+export 'view/widgets/widgets.dart';
