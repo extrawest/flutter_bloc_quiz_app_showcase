2173d74a4bcd4ee2bf91490707902b040aad2fcd
Maksym Ostrovyi <maksym.ostrovyj@extrawest.com>
2023-04-24 11:23:34 +0300

    unused code deleted

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:bloc_quiz_training/common/router.dart';
 import 'package:bloc_quiz_training/common/services/credentials_loader.dart';
 import 'package:bloc_quiz_training/common/utils/app_utils.dart';
-import 'package:bloc_quiz_training/features/posts/repositories/posts_repository.dart';
-import 'package:bloc_quiz_training/features/posts/services/posts_api_service.dart';
 import 'package:bloc_quiz_training/features/theme/bloc/theme_bloc.dart';
 import 'package:bloc_quiz_training/features/theme/theme_info.dart';
 import 'package:easy_localization/easy_localization.dart';
@@ -16,12 +14,8 @@ import 'package:url_strategy/url_strategy.dart';
 import 'common/assets.dart';
 import 'common/services/database_boxes.dart';
 import 'common/utils/logger.dart';
-import 'features/one_answer_quiz/repositories/one_answer_quiz_repository.dart';
-import 'features/one_answer_quiz/services/one_answer_quiz_api_service.dart';
-import 'features/storage/data_models/quiz_result_model.dart';
-import 'features/storage/quiz_storage_bloc.dart';
-import 'features/true_false_quiz/repositories/true_false_quiz_repository.dart';
-import 'features/true_false_quiz/services/true_false_quiz_api_service.dart';
+import 'features/features.dart';
+import 'features/storage/storage.dart';
 import 'network/api_client.dart';
 
 const isProductionEnvKey = 'IS_PRODUCTION';
@@ -98,13 +92,6 @@ class _RepositoriesHolder extends StatelessWidget {
         RepositoryProvider(
           create: (context) => TrueFalseQuizRepositoryImpl(
             trueFalseQuizApiService: TrueFalseQuizApiServiceImpl(
-              getIt<ApiClientImpl>(),
-            ),
-          ),
-        ),
-        RepositoryProvider(
-          create: (context) => PostsRepositoryImpl(
-            postsApiService: PostsApiServiceImpl(
               getIt<ApiClientImpl>(),
             ),
           ),
