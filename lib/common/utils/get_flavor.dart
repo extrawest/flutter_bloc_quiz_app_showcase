58a5cc8642e3bdcdb73ee289cf668de41bfaa4f5
Maksym Ostrovyi <maksym.ostrovyj@extrawest.com>
2023-04-26 18:12:20 +0300

    device_preview added

--- lib/common/utils/get_flavor.dart
+++ lib/common/utils/get_flavor.dart
@@ -1,6 +1,10 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 
 Future<String?> getFlavor() async {
+  if (kIsWeb) {
+    return 'prod';
+  }
   const channel = MethodChannel('flavor_channel');
   final flavor = await channel.invokeMethod<String>('getFlavor');
   return flavor;
--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,9 @@ import 'package:bloc_quiz_training/common/services/credentials_loader.dart';
 import 'package:bloc_quiz_training/common/utils/app_utils.dart';
 import 'package:bloc_quiz_training/features/theme/bloc/theme_bloc.dart';
 import 'package:bloc_quiz_training/features/theme/theme_info.dart';
+import 'package:device_preview/device_preview.dart';
 import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -109,15 +111,19 @@ class Application extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocBuilder<ThemeBloc, ThemeState>(
       builder: (context, state) {
-        return MaterialApp.router(
-          title: 'Flutter BLoC Starter',
-          routerConfig: goRouter,
-          darkTheme: darkTheme,
-          theme: state.themeData,
-          themeMode: state.themeMode,
-          localizationsDelegates: context.localizationDelegates,
-          supportedLocales: context.supportedLocales,
-          locale: context.locale,
+        return DevicePreview(
+          enabled: kIsWeb,
+          builder: (context) => MaterialApp.router(
+            useInheritedMediaQuery: true,
+            title: 'Simple Quiz',
+            routerConfig: goRouter,
+            darkTheme: darkTheme,
+            theme: state.themeData,
+            themeMode: state.themeMode,
+            localizationsDelegates: context.localizationDelegates,
+            supportedLocales: context.supportedLocales,
+            locale: context.locale,
+          ),
         );
       },
     );
